9058f32c76bab59a9a9060d5bee1e337287c4f7a
Limsukhyun <as17269@naver.com>
2023-10-18 17:41:57 +0900

    feat: 3D 추가

--- src/main/java/com/example/demo/service/threeServiceImpl.java
+++ src/main/java/com/example/demo/service/threeServiceImpl.java
@@ -1,17 +1,32 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.example.demo.dto.dtoClass;
+import com.example.demo.mapper.threeMapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class threeServiceImpl implements threeService {
+    private final threeMapper threeMapper;
+
+    @Autowired
+    public threeServiceImpl(threeMapper threeMapper) {
+        this.threeMapper = threeMapper;
+    }
 
     public String processData(dtoClass data) {
         try {
+            // threeMapper를 사용하여 데이터베이스에서 데이터 조회
+            List<dtoClass> results = threeMapper.selectYourEntities();
+            
             // ObjectMapper를 사용하여 DTO 객체를 JSON 문자열로 변환
             ObjectMapper objectMapper = new ObjectMapper();
-            String json = objectMapper.writeValueAsString(data);
+            String json = objectMapper.writeValueAsString(results);
+            // String json = objectMapper.writeValueAsString(data);
 
             // JSON 문자열을 반환
             return json;
