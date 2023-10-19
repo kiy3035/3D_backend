89166ea5a271251ffe388857961e4da92999c2ea
김인영 <116708377+kiy3035@users.noreply.github.com>
2023-10-19 10:20:53 +0900

    feat: 3D 추가

--- src/main/java/com/example/demo/mapper/threeMapper.java
+++ src/main/java/com/example/demo/mapper/threeMapper.java
@@ -1,5 +1,7 @@
 package com.example.demo.mapper;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
@@ -8,4 +10,8 @@ import com.example.demo.dto.dtoClass;
 @Mapper
 public interface threeMapper {
     List<dtoClass> selectYourEntities(); // 예제 메서드
+
+    // LinkedHashMap<String, Object> selectTest();
+
+
 }
--- src/main/java/com/example/demo/service/threeServiceImpl.java
+++ src/main/java/com/example/demo/service/threeServiceImpl.java
@@ -19,18 +19,21 @@ public class threeServiceImpl implements threeService {
     }
 
     public String processData(dtoClass data) {
+        // threeMapper.selectTest();
         try {
+            System.out.println("1111111111111111");
             // threeMapper를 사용하여 데이터베이스에서 데이터 조회
-            List<dtoClass> results = threeMapper.selectYourEntities();
-            
+            threeMapper.selectYourEntities();
+            System.out.println("2222222222222222");
             // ObjectMapper를 사용하여 DTO 객체를 JSON 문자열로 변환
-            ObjectMapper objectMapper = new ObjectMapper();
-            String json = objectMapper.writeValueAsString(results);
+            // ObjectMapper objectMapper = new ObjectMapper();
+            // String json = objectMapper.writeValueAsString(results);
             // String json = objectMapper.writeValueAsString(data);
 
             // JSON 문자열을 반환
-            return json;
-        } catch (Exception e) {
+            return null;
+        } 
+        catch (Exception e) {
             e.printStackTrace();
             // 오류 처리
             return "오류 발생";
