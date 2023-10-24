e56a3b12e26a92aae65387d6f2d9416006286e17
김인영 <116708377+kiy3035@users.noreply.github.com>
2023-10-24 10:56:00 +0900

    feat: 3D 추가

--- src/main/java/com/example/demo/service/threeServiceImpl.java
+++ src/main/java/com/example/demo/service/threeServiceImpl.java
@@ -8,24 +8,30 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.dto.dtoClass;
 import com.example.demo.mapper.threeMapper;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class threeServiceImpl implements threeService {
     
     private final threeMapper threeMapper;
-
+    
     @Autowired
     public threeServiceImpl(threeMapper threeMapper) {
         this.threeMapper = threeMapper;
     }
 
-    public String inputData(dtoClass data) {
-        
+    public String inputData(List<dtoClass> dataList) {
         try {
             System.out.println("1111111111111111");
-            threeMapper.insertMst(data);
-            threeMapper.insertDtl(data);
+             // 첫 번째 데이터만을 대상으로 mst 호출
+             if (!dataList.isEmpty()) {
+                threeMapper.insertMst(dataList.get(0));
+            }
+            
+            // 모든 데이터에 대해 dtl 호출
+            for (dtoClass data : dataList) {
+                // 각 data 객체를 MyBatis 매퍼로 전달
+                threeMapper.insertDtl(data); // 여기서 발생 
+            }
             System.out.println("2222222222222222");
 
             return null;
